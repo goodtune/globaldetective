a1f71956d809b2993dc4131f1e9bb661ed51d5f4
Gary Reynolds <gary@touch.asn.au>
2024-12-14 12:11:07 +1100

    feature: add `Country` and `City` models to `places` app

--- places/admin.py
+++ places/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from places.models import City, Country
+
+
+class CountryAdmin(admin.ModelAdmin):
+    list_display = ("name", "code")
+
+
+class CityAdmin(admin.ModelAdmin):
+    list_display = ("name", "country")
+
+
+admin.site.register(Country, CountryAdmin)
+admin.site.register(City, CityAdmin)
