503497050ef7956f150839c7fc11a8464ad81ea5
Gary Reynolds <gary@touch.asn.au>
2024-12-14 20:37:50 +1100

    feature: initial page of `factbook`

--- factbook/views.py
+++ factbook/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+from places.models import Country
+
+
+def index(request):
+    countries = Country.objects.all()
+    context = {"countries": countries}
+    return render(request, "factbook/index.html", context)
