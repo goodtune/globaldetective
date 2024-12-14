295202d34b270ca2aafdfb7997d9f5248491d22d
Gary Reynolds <gary@touch.asn.au>
2024-12-14 14:38:09 +1100

    chore: add fields for clues about countries

--- places/admin.py
+++ places/admin.py
@@ -4,11 +4,14 @@ from places.models import City, Country
 
 
 class CountryAdmin(admin.ModelAdmin):
-    list_display = ("name", "code")
+    list_display = ("name", "common_name", "code", "currency")
+    list_editable = ("common_name", "currency")
+    list_filter = ("currency",)
 
 
 class CityAdmin(admin.ModelAdmin):
-    list_display = ("name", "country")
+    list_display = ("name", "country__common_name", "country__currency")
+    list_filter = ("country__currency",)
 
 
 admin.site.register(Country, CountryAdmin)
--- places/models.py
+++ places/models.py
@@ -2,8 +2,14 @@ from django.db import models
 
 
 class Country(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField("Official Name", max_length=100)
+    common_name = models.CharField("Common Name", max_length=50, blank=True, null=True)
     code = models.CharField(max_length=2)
+    currency = models.CharField(max_length=20, blank=True, null=True)
+    flora = models.TextField(blank=True, null=True)
+    fauna = models.TextField(blank=True, null=True)
+    geography = models.TextField(blank=True, null=True)
+    exports = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.name
